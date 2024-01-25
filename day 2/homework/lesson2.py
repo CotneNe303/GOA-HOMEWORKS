c386be89e7858afdc2e8775486762946cb23f1d9
CotneNe303 <cotnemail.com@gmail.com>
2024-01-25 04:44:14 -0800

    second commit

---
+++ day 2/homework/lesson2.py
@@ -0,0 +1,33 @@
+name = "cotne"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობისთვის მიმნიჭებელი სიმბოლო
+# "cotne" არის ცვლადის მნიშვნელობა
+
+surname = "nebieridze"
+
+# print(name)
+#print ფუნქციას გადაეცემა ეკრანზე გამოსატანი ცვლადი ობიქეტი
+
+name = "cotne" #ეს არის str (string) ტიპის ცვლადი
+age = 15 # ეს არის int (integer) მთელი რიცხვი
+height = 182 #ეს არის float ტიპის ცვლადი ( ათწილადი)
+#boolean (bool) ტიპის ცვლადი
+
+knows_programing = True  #True  ან False
+is_ugly = False #snakecase(უბრალოდ წერის სტილი სტანდარტულად)
+isUgly = False #ჯავასკრიპტული  camelcase
+
+
+
+#print(name + " " + surname)
+
+# სტრინგი არის  ბრჭყალებში მოქცეული სიმბოლოები
+#print (name + age)
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programing))
+
+print(  name + " " + surname + " " + str(age) + " " + str(height))
