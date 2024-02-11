e635a3f1dac92c96c50e4ed83befa99d1d00a7cc
CotneNe303 <cotnemail.com@gmail.com>
2024-02-11 11:10:56 -0800

    day 08

---
+++ day 03/classwork.py
@@ -0,0 +1,29 @@
+print("hello world")
+
+print("7+2")
+
+age = 17
+
+print(age +10)
+
+x =5
+
+print(x + 1)
+
+
+age =15
+
+print(age +10)
+
+
+age =20
+
+print(age +10)
+
+#re initialize 
+
+
+num1 = 5+5
+num2 =10 - 3
+
+print(num1 + num2)
