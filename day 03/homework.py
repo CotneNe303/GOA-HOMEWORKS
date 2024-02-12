c380e4b9dca7d1cb317535b25f6c7dc992c9b427
CotneNe303 <cotnemail.com@gmail.com>
2024-02-12 01:46:24 -0800

    day 03

---
+++ day 03/homework.py
@@ -0,0 +1,30 @@
+
+name ="deda"
+age =42
+height =160
+print( name + " " + str(age) + " " + str(height) )
+
+#ახლანდელი დრო
+
+name ="deda"
+age =42+2
+height =160
+print( name + " " + str(age) + " " + str(height) )
+
+#ორიწლის მერე
+
+name ="mama"
+age =45
+height =177
+print( name + " " + str(age) + " " + str(height) )
+
+#ახლანდელი დრო
+
+name ="mama"
+age =45+2
+height =177
+print( name + " " + str(age) + " " + str(height) )
+
+
+#ორიწლის მერე
+
