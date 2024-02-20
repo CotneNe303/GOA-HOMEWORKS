0121036e3f43eee81e267de24575ad602db28788
CotneNe303 <cotnemail.com@gmail.com>
2024-02-20 14:28:30 +0400

    day05

---
+++ day 05/homework.py
@@ -0,0 +1,13 @@
+num1 = int(input("enter any number: " ))
+num2 = int(input("enter any number: "))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 < num2)
+print(num1 > num2)
+print(num1 == num2)
+print(num1 != num2)
+print(num1 >= num2)
+print(num1 <= num2)
