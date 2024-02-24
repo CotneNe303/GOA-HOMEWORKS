1bca1a32d26f6a1b54738e1dce69ee7ef483c9a9
CotneNe303 <cotnemail.com@gmail.com>
2024-02-24 22:19:23 +0400

    day06

---
+++ day 06/classwork.py
@@ -0,0 +1,24 @@
+num1 =int(input("please enter first number: "))
+num2 =int(input("please enter second number: "))
+
+#math operation
+
+print("the result of + is :", num1 + num2)
+print("the result of - is :", num1 - num2)
+print("the result of * is :", num1 * num2)
+print("the result of / is :", num1 / num2)
+
+
+# math comperations
+
+print("the result of > is :", num1 > num2)
+print("the result of < is :", num1 < num2)
+print("the result of >= is :", num1 >= num2)
+print("the result of <= is :", num1 <= num2)
+print("the result of == is :", num1 == num2) 
+
+
+
+print(10 / 3)# this / operatoe will divide accuracy to another
+print(10 // 3)# this // operator will return only whole result
+print(10 % 3)# this % will return remainder  
---
+++ day 06/classwork2.py
@@ -0,0 +1,7 @@
+name = input("whats your firstname ? :")
+lastname =input("whats your lastname ? :")
+age =int(input("whats your age ? :"))
+height =float(input("whats your height ? :"))
+
+print(f"hello, i am {name} {lastname} and i am {age} years old and i am {height} tall.")
+
