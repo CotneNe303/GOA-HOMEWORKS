4340c572f241aa1ed28c95a396ef9572ea8f868b
CotneNe303 <cotnemail.com@gmail.com>
2024-02-28 00:12:16 +0400

    day07

---
+++ day07/homework.py
@@ -0,0 +1,25 @@
+print('### FOR 1 ###')
+sum = 0
+for i in range(10):
+   if i %2==0:
+      print(i)
+
+
+print('### WHILE 1 ###')
+index = 0
+while (index <= 20):
+   print(index)
+   index += 1
+
+print('### WHILE 2 ###')
+index = 0
+sum =0
+while (index <= 5):
+   sum += index
+   index += 1
+   print(index)
+print(sum)
+
+print('### FOR 2 ###')
+for i in "apple":
+   print(i)
