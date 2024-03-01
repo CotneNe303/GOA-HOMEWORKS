182a80f2a872eb53e06643104dbeaecd8009cb28
CotneNe303 <cotnemail.com@gmail.com>
2024-03-01 23:52:58 +0400

    day08

---
+++ day08/classwor2.py
@@ -0,0 +1,7 @@
+age =input("whats your age?: ")
+if int(age) >= 18:
+    print("sruwlovani")
+else:
+    print("arasrulwlovani")
+
+
---
+++ day08/classwork.py
@@ -0,0 +1,49 @@
+# 1.
+#for i in range(0,10,2):
+   # print(i)
+
+#2. 
+
+#i = 1
+
+# while i < 20:
+#     i = i+1
+#     print(i)
+
+#3.
+
+# result = 0 
+# i = 0
+
+# while i <= 5:
+#     result = result + i 
+#     i = i + 1
+
+# print(result)
+
+# 4.
+
+# name = "cotne"
+
+# for i in name:
+#     print(i)
+
+
+
+
+# for i in range(5):
+#     print("cotne")
+
+# print('1234566')
+
+# print(5 != 6)
+# print(5 != 6)   
+
+#condittional statemants
+
+passed_minutes = 50
+if passed_minutes >= 50:
+    print("davisvenot")
+else: 
+    print("ar davisvenot")
+
