bd8904cb36271681c84335ea6bcbaedbf26be9b4
CotneNe303 <cotnemail.com@gmail.com>
2024-03-05 13:11:16 +0400

    day09

---
+++ day 09/classwokr.py
@@ -0,0 +1,6 @@
+# num =int(input("enter number"))
+# print(num > 1 and num  <10)
+
+
+num =int(input("enter number"))
+print(num %5 == 0 and num %10 == 0)
---
+++ day 09/homework.py
@@ -0,0 +1,66 @@
+#1) დავალება შევამოწმოთ უყოფა თუ არა მომხმარებლის მიერ შემოტანილი რიცხვი 2ზე და 4ზე
+num = int(input("please enter any number: "))
+print(num %2 == 0  and num %4 == 0)
+
+#2) გავაკეთოთ 10 მაგალითი შედარების ოპერატორებზე( > < >= <= == !=)
+num1 = int(input("pelase enter any number: "))
+print(num1 > 5)
+
+num2 = int(input("pelase enter any number: "))
+print(num2 < 8)
+
+num3 = int(input("pelase enter any number: "))
+print(num3 >= 14)
+
+num4 = int(input("pelase enter any number: "))
+print(num4 <= 23 )
+
+num5 = int(input("pelase enter any number: "))
+print(num5) == 12
+
+num6 = int(input("pelase enter any number: "))
+print(num6 != 7)
+
+num7 = int(input("pelase enter any number: "))
+print(num7 == 9)
+
+num8 = int(input("pelase enter any number: "))
+print(num8 <= 12)
+
+num9 = int(input("pelase enter any number: "))
+print(num9 >= 9)
+
+num0 = int(input("pelase enter any number: "))
+print(num0 > 10)
+
+#3) გავაკეთოთ 10 მაგალითი "and" ოპერატორზე
+
+num =int(input("enter number"))
+print(num %4 == 0 and num %3 == 0)
+
+num =int(input("enter number"))
+print(num %5 == 0 and num %2 == 0)
+
+num =int(input("enter number"))
+print(num %2 == 0 and num %4 == 0)
+
+num =int(input("enter number"))
+print(num %5 == 0 and num %10 == 0)
+
+num =int(input("enter number"))
+print(num %3 == 0 and num %9 == 0)
+
+num =int(input("enter number"))
+print(num %2 == 0 and num %8 == 0)\
+
+num =int(input("enter number"))
+print(num %2 == 0 and num %6 == 0)
+
+num =int(input("enter number"))
+print(num %3 == 0 and num %9 == 0)
+
+num =int(input("enter number"))
+print(num %7 == 0 and num %8 == 0)
+
+num =int(input("enter number"))
+print(num %9 == 0 and num %1 == 0)
