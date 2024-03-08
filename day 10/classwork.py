3c0c4014e290b2f7d4c30f6442c56dc309f4b40b
CotneNe303 <cotnemail.com@gmail.com>
2024-03-08 12:00:01 +0400

    day10

---
+++ day 10/classwork.py
@@ -0,0 +1,33 @@
+# num = int(input("please enter number: "))
+
+# # print(num % 3 == 0 or num % 5 == 0 )
+
+# if num > 0:
+#     print("number is positive")
+# elif num < 0:
+#     print("number is negative")
+# else:
+#     print("zero")
+
+
+# age = int(input("whats your age? : "))
+
+# if age >= 0 and age < 13:
+#     print("you are child")
+# elif age >= 13 and age < 18:
+#     print("you are teenager")
+# elif age >= 18 and age < 60: 
+#     print("adult")
+# else:
+#     print("old")
+
+
+
+favourite_movies = ["twd", "the flash","matrix"]
+
+print(favourite_movies)
+print(favourite_movies[2])
+
+numbers = [1, 2, 3, 4, 5]
+print(numbers[4])
+print(numbers[0],numbers[1])
---
+++ day 10/homework.py
@@ -0,0 +1,21 @@
+#1)
+programing_list =["java", "css", "html", "c++", "javascript"]
+print(programing_list)
+print(programing_list[4])
+
+#2)
+list =["color", 15, 15.99, True]
+print(list[0])
+print(list[1])
+print(list[2])
+print(list[3])
+
+
+#3)
+ოთხისჯერადები = [4, 8, 12, 16, 20]
+print(ოთხისჯერადები[4])
+
+#4)
+კენტები =[31, 33, 35, 37, 39, 41, 43, 45, 47, 49]
+print(კენტები[0] * კენტები[1] * კენტები[2])
+print(კენტები[1], კენტები[4], კენტები[7])
