e0eeb810a1dc7d224ab989f9d468b6f30cf91075
CotneNe303 <cotnemail.com@gmail.com>
2024-03-14 20:51:28 +0400

    day12

---
+++ day 12/classwork.py
@@ -0,0 +1,61 @@
+# shopping_list = ["apple", 5, "wood", True]
+
+# shopping_list.pop(2)      #append da pop (ჩამატება და ამოღება)         
+
+# print(shopping_list) 
+
+
+# numbers = [1,2,3,4,5,6,7,8,9,10]
+
+# for i in numbers:
+
+# numbers = range(5,10)
+
+# name = "luka"
+
+ # print(name[3])
+
+# for i in name:
+#     print(i)
+
+
+# numbers = [1,2,3,4,5,6,7,8,9,10]
+
+# for i in numbers:
+#      if i % 2 != 0:                           #ამოგდების გზით
+#           index = numbers.index(i)
+#           numbers.pop(index)
+
+# print(numbers)
+
+
+# numbers = [1,2,3,4,5,6,7,8,9,10]
+# even_numbers = []
+
+# for i in numbers:
+#     if i % 2 == 0:                            #ჩამატების გზით (პროფები)
+#         even_numbers.append(i)
+
+# print(even_numbers)
+
+
+
+# names = ("nika", "luka", "dato", "gabrieli", "lile")
+
+# after_slice = names[2:]
+# print(after_slice)
+
+
+
+sia = ["lasha", "gio", "luka"]
+print(sia[1:3])
+
+sityva = "mouse"
+print(sityva[2:4])
+
+
+name = "ცოტნე ნებიერიძე"
+print(name[0:4])
+
+meore_sia = ["lasha", "gio", "luka"]
+print(meore_sia[-1:-3])
