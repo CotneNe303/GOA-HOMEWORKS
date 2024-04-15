ce8df81456201f5dc3ecc1bb40b277385de79d25
CotneNe303 <cotnemail.com@gmail.com>
2024-04-15 23:20:05 +0400

    day 21

---
+++ day 20/classwork/classwork.py
@@ -0,0 +1,48 @@
+# my_list = [1,2,3,4]
+# # my_list.append(5)
+# # print(my_list)
+
+# print(len(my_list))
+
+
+# listn = [1,2,3,4,5,6,7,8,9,10]
+# new_list = []
+# for i in range(len(listn)):
+#     if i % 2 == 0:
+#         new_list.append(i)
+# print(new_list)
+        
+# my_list = [12,24,55,68,79,25]
+# even_list = []
+# odd_list = []
+
+# for i in range(len(my_list)):
+#     if my_list[i] % 2 == 0:
+#         even_list.append(my_list[i])
+#     else:
+#         odd_list.append(my_list[i])
+
+#append
+#pop
+#len
+#insert
+
+# my_list = [14,56,79,84,53,4,5]
+
+# for i in range(len(my_list)):
+#     if my_list % 2 != 0:
+#         my_list.pop(i)
+        
+# print(my_list)
+
+# my_list = [14,56,79,84,53,4,5]
+# # my_list.pop(2)
+# # print(my_list.pop(2))
+
+# for i in range(len(my_list) - 1):
+#     # if my_list[i] % 2 != 0:
+#     #     #  my_list.pop(i)
+#     #     print(my_list[i])
+#     #     print(i, "index")
+#         my_list.pop(i)
+# print(my_list)
