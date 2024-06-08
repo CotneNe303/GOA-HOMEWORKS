ec2fd9e338c2f6e4ff6a9bc2a3bf763e1a9e28c8
CotneNe303 <cotnemail.com@gmail.com>
2024-06-08 22:53:38 +0400

    day37

---
+++ day 36/classwork/classwork(dictionary).py
@@ -0,0 +1,63 @@
+# #dictionary
+# person = {
+#     #key     value
+#     "name": "davit",
+#     "surname": "grdzelishvili",
+#     "age": "20",
+#     "arr": [1,2,3,4,5]
+# }
+
+
+# person["name"] = "giorgi"
+
+# print(person)
+
+
+# print(person["name"], "/ arr მეთოდი")
+# # გვიბურნებს valueებს
+# print(person.get("name"), "get method")
+# #გვიბრუნებს valuebsებს
+# print(person.keys())
+# #გვიბრუნებს keyებს
+
+# print(person.values())
+# #გვიბრუნებს valueებს
+
+# print(person.items())
+# #გვუბრუნებს წყვილს tuplesეებს
+
+# #ამატებს და ცვლის itemებს
+# person["food"] = "mtsvadi"
+
+# print(person)
+
+# person.update({"name": "daviti"})
+
+# #itemების ამოგდება
+# person.pop("age")
+# print(person)
+# del person['name']
+# person.clear()
+# print(person)
+
+# print(person["arr"][0])
+
+# x =person["arr"]
+# print(x)
+
+# print(len(person))
+
+
+# about_Me ={
+#     "name": "cotne",
+#     "lastname": "nebieridze",
+#     "age": 15,
+#     "height": "185"
+# }
+# make_join = about_Me.values
+# values_join = " ". join([str(i) for i in make_join.join()])
+
+# print(values_join)
+
+
+
