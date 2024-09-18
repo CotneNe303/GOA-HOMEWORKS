120b8528b87b2145bd85008917c139d50e18c82a
CotneNe303 <cotnemail.com@gmail.com>
2024-09-18 14:49:44 +0400

    day66

---
+++ day 66/homework/script.js
@@ -0,0 +1,29 @@
+//#1
+// const div = document.querySelector('#parent');
+
+// const newP = document.createElement('p');
+// newP.textContent = "Hello world"
+
+// div.appendChild(newP);
+//#2
+// const CDiv = div.cloneNode(ture)
+// document.body.appendChild(CDiv);
+
+
+//#3
+// const uL = document.querySelector("#UL")
+// uL.removeChild(uL.children[3])
+
+//#4
+
+// const newE = document.createElement("p");
+// newE.textContent = "HELLO World"
+
+// div.replaceChild(newE, div.firstElementChild);
+
+
+//#5
+// uL.firstElementChild.remove();
+
+
+
